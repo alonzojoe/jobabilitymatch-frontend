70a3c9754a6f3debd8d4cfee28d712e7f5579bb0
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-04-28 23:50:38 +0800

    feat: create partial ui for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,8 +16,90 @@ const Dashboard = () => {
       </ol>
 
       <h1 className="page-header">
-        Page Header <small>header small text goes here...</small>
+        Dashboard <small>header small text goes here...</small>
       </h1>
+
+      <div className="row">
+        {/* begin col-3 */}
+        <div className="col-xl-3 col-md-6">
+          <div className="widget widget-stats bg-white text-inverse">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-person-add"></i>
+            </div>
+            <div className="stats-content">
+              <div className="text-inverse font-weight-bold">Users</div>
+              <div className="stats-number">7,842,900</div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "70.1%" }}></div>
+              </div>
+              <div className="stats-desc text-inverse-lighter">
+                Better than last week (70.1%)
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end col-3 */}
+
+        {/* begin col-3 */}
+        <div className="col-xl-3 col-md-6">
+          <div className="widget widget-stats bg-white text-inverse">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-business"></i>
+            </div>
+            <div className="stats-content">
+              <div className="text-inverse font-weight-bold">Companies</div>
+              <div className="stats-number">180,200</div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "40.5%" }}></div>
+              </div>
+              <div className="stats-desc text-inverse-lighter">
+                Better than last week (40.5%)
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end col-3 */}
+
+        {/* begin col-3 */}
+        <div className="col-xl-3 col-md-6">
+          <div className="widget widget-stats bg-white text-inverse">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-create"></i>
+            </div>
+            <div className="stats-content">
+              <div className="text-inverse font-weight-bold">Job Postings</div>
+              <div className="stats-number">38,900</div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "76.3%" }}></div>
+              </div>
+              <div className="stats-desc text-inverse-lighter">
+                Better than last week (76.3%)
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end col-3 */}
+
+        {/* begin col-3 */}
+        <div className="col-xl-3 col-md-6">
+          <div className="widget widget-stats bg-white text-inverse">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-walk"></i>
+            </div>
+            <div className="stats-content">
+              <div className="text-inverse font-weight-bold">Applicants</div>
+              <div className="stats-number">3,988</div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "54.9%" }}></div>
+              </div>
+              <div className="stats-desc text-inverse-lighter">
+                Better than last week (54.9%)
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end col-3 */}
+      </div>
 
       <div className="panel panel-inverse">
         <div className="panel-heading">
