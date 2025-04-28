52d294101c8afd04e9d67722a1457e98ed3642a2
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-28 16:47:32 +0800

    feat: apply dynamic menus in Sidebar component and separate NavItem

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,57 +3,57 @@ import React from "react";
 const Dashboard = () => {
   return (
     <>
-      <ol class="breadcrumb float-xl-right">
-        <li class="breadcrumb-item">
+      <ol className="breadcrumb float-xl-right">
+        <li className="breadcrumb-item">
           <a href="javascript:;">Home</a>
         </li>
-        <li class="breadcrumb-item">
+        <li className="breadcrumb-item">
           <a href="javascript:;">Library</a>
         </li>
-        <li class="breadcrumb-item active">
+        <li className="breadcrumb-item active">
           <a href="javascript:;">Data</a>
         </li>
       </ol>
 
-      <h1 class="page-header">
+      <h1 className="page-header">
         Page Header <small>header small text goes here...</small>
       </h1>
 
-      <div class="panel panel-inverse">
-        <div class="panel-heading">
-          <h4 class="panel-title">Panel Title here</h4>
-          <div class="panel-heading-btn">
+      <div className="panel panel-inverse">
+        <div className="panel-heading">
+          <h4 className="panel-title">Panel Title here</h4>
+          <div className="panel-heading-btn">
             <a
               href="javascript:;"
-              class="btn btn-xs btn-icon btn-circle btn-default"
+              className="btn btn-xs btn-icon btn-circle btn-default"
               data-click="panel-expand"
             >
-              <i class="fa fa-expand"></i>
+              <i className="fa fa-expand"></i>
             </a>
             <a
               href="javascript:;"
-              class="btn btn-xs btn-icon btn-circle btn-success"
+              className="btn btn-xs btn-icon btn-circle btn-success"
               data-click="panel-reload"
             >
-              <i class="fa fa-redo"></i>
+              <i className="fa fa-redo"></i>
             </a>
             <a
               href="javascript:;"
-              class="btn btn-xs btn-icon btn-circle btn-warning"
+              className="btn btn-xs btn-icon btn-circle btn-warning"
               data-click="panel-collapse"
             >
-              <i class="fa fa-minus"></i>
+              <i className="fa fa-minus"></i>
             </a>
             <a
               href="javascript:;"
-              class="btn btn-xs btn-icon btn-circle btn-danger"
+              className="btn btn-xs btn-icon btn-circle btn-danger"
               data-click="panel-remove"
             >
-              <i class="fa fa-times"></i>
+              <i className="fa fa-times"></i>
             </a>
           </div>
         </div>
-        <div class="panel-body">Panel Content Here</div>
+        <div className="panel-body">Panel Content Here</div>
       </div>
     </>
   );
