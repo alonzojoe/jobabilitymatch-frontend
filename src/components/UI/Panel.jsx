c3c66cdff596e6f97e96c8598de40dfe5921eecd
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-30 16:59:18 +0800

    feat: create ui for disability types tables and pagination

---
+++ src/components/UI/Panel.jsx
@@ -0,0 +1,42 @@
+const Panel = ({ title = "", children }) => {
+  return (
+    <div className="panel panel-inverse">
+      <div className="panel-heading">
+        <h4 className="panel-title">{title}</h4>
+        <div className="panel-heading-btn">
+          <a
+            href="javascript:;"
+            className="btn btn-xs btn-icon btn-circle btn-default"
+            data-click="panel-expand"
+          >
+            <i className="fa fa-expand"></i>
+          </a>
+          <a
+            href="javascript:;"
+            className="btn btn-xs btn-icon btn-circle btn-success"
+            data-click="panel-reload"
+          >
+            <i className="fa fa-redo"></i>
+          </a>
+          <a
+            href="javascript:;"
+            className="btn btn-xs btn-icon btn-circle btn-warning"
+            data-click="panel-collapse"
+          >
+            <i className="fa fa-minus"></i>
+          </a>
+          <a
+            href="javascript:;"
+            className="btn btn-xs btn-icon btn-circle btn-danger"
+            data-click="panel-remove"
+          >
+            <i className="fa fa-times"></i>
+          </a>
+        </div>
+      </div>
+      <div className="panel-body">{children}</div>
+    </div>
+  );
+};
+
+export default Panel;
--- src/pages/DisabilityTypes/DisabilityTypes.jsx
+++ src/pages/DisabilityTypes/DisabilityTypes.jsx
@@ -1,7 +1,87 @@
-import React from "react";
-
+import PageHeader from "@/components/General/PageHeader";
+import Panel from "@/components/UI/Panel";
 const DisabilityTypes = () => {
-  return <div>Disability Types</div>;
+  return (
+    <>
+      <PageHeader title="Disability Types" />
+      <Panel>
+        <div className="row">
+          <div className="col-12">
+            <div className="table-responsive">
+              <table className="table table-striped table-bordered table-td-valign-middle dataTable no-footer dtr-inline collapsed">
+                <thead>
+                  <tr>
+                    <th className="text-center font-weight-bold">ID</th>
+                    <th className="text-center font-weight-bold">
+                      Disablity Type
+                    </th>
+                    <th className="text-center font-weight-bold">Option</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="text-center">1.</td>
+                    <td className="text-center">Hearing Impairment</td>
+                    <td className="text-center">Win 95+</td>
+                  </tr>
+                  <tr>
+                    <td className="text-center">2.</td>
+                    <td className="text-center">Hearing Impairment</td>
+                    <td className="text-center">Win 95+</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="mt-2 pagination-container m-0 p-3 d-flex justify-content-between align-items-center">
+                <div className="fs-7">Total Pages: 0</div>
+                <div className="fs-7">
+                  Page <span>0 of 0</span>
+                </div>
+                <div>
+                  <ul className="pagination" style={{ marginBottom: "0" }}>
+                    <li className="paginate_button page-item previous disabled">
+                      <a href="#" className="page-link">
+                        Previous
+                      </a>
+                    </li>
+                    <li className="paginate_button page-item active">
+                      <a href="#" className="page-link">
+                        1
+                      </a>
+                    </li>
+                    <li className="paginate_button page-item">
+                      <a href="#" className="page-link">
+                        2
+                      </a>
+                    </li>
+                    <li className="paginate_button page-item">
+                      <a href="#" className="page-link">
+                        3
+                      </a>
+                    </li>
+                    <li className="paginate_button page-item">
+                      <a href="#" className="page-link">
+                        4
+                      </a>
+                    </li>
+                    <li className="paginate_button page-item">
+                      <a href="#" className="page-link">
+                        5
+                      </a>
+                    </li>
+                    <li className="paginate_button page-item next">
+                      <a href="#" className="page-link">
+                        Next
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Panel>
+    </>
+  );
 };
 
 export default DisabilityTypes;
