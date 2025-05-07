be90dd3723fe82a976c287020702e8eddafc0278
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-05-07 09:59:37 +0800

    feat: configure logic in Protected route guard

--- src/middleware/ProtectedRoutes.jsx
+++ src/middleware/ProtectedRoutes.jsx
@@ -1,7 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import useAuthSetup from "@/hooks/useAuthSetup";
+import { getLocalStorage } from "@/libs/utils";
+
 const ProtectedRoutes = () => {
-  useAuthSetup();
+  const { isLoading } = useAuthSetup();
+  const authUser = getLocalStorage("auth-user");
+
+  if (isLoading) {
+    return <div className="flicker-screen"></div>;
+  }
+
+  if (!authUser) {
+    return (window.location.href = "/");
+  }
+
+  if (authUser.role_id === 2) {
+    return <Navigate to="/unauthorized" replace />;
+  }
+
   return <Outlet />;
 };
 
