af7b4461ca68a66b9c13e0a55d66b5e2857ad668
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-05-13 14:17:13 +0800

    feat: implement api endpoint dashboard/admin data.

--- src/pages/Dashboard/components/MainDashboard.jsx
+++ src/pages/Dashboard/components/MainDashboard.jsx
@@ -1,142 +1,199 @@
-import PageHeader from "@/components/Global/PageHeader";
+import React, { useContext } from "react";
+import Panel from "@/components/UI/Panel";
+import AuthContext from "@/store/auth/auth-context";
+import { Link } from "react-router-dom";
+import useFetch from "@/hooks/useFetch";
+import { formatCount } from "@/libs/utils";
+import { Bar, Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const MainDashboard = () => {
+  const { authUser } = useContext(AuthContext);
+  const { data: dashboardData } = useFetch(`/dashboard/admin`, null);
+
+  // console.log("dashboard", dashboardData?.data);
+  // const company = authUser?.company?.name ?? "";
+
+  // const labels = [
+  //   "Job Postings",
+  //   "Applicants",
+  //   "Hired Applicants",
+  //   "Rejected Applicants",
+  // ];
+
+  // const barChartData = {
+  //   labels,
+  //   datasets: [
+  //     {
+  //       label: "Company Statistics",
+  //       data: [
+  //         dashboardData?.data?.job_postings || 0,
+  //         dashboardData?.data?.applicants || 0,
+  //         dashboardData?.data?.accepted_applicants || 0,
+  //         dashboardData?.data?.rejected_applicants || 0,
+  //       ],
+  //       backgroundColor: ["#FF9500", "#5AC8FA", "#FF3B30", "#B503FF"],
+  //     },
+  //   ],
+  // };
+
+  // const pieChartData = {
+  //   labels,
+  //   datasets: [
+  //     {
+  //       label: "Company Distribution",
+  //       data: [
+  //         dashboardData?.data?.job_postings || 0,
+  //         dashboardData?.data?.applicants || 0,
+  //         dashboardData?.data?.accepted_applicants || 0,
+  //         dashboardData?.data?.rejected_applicants || 0,
+  //       ],
+  //       backgroundColor: ["#FF9500", "#5AC8FA", "#FF3B30", "#B503FF"],
+  //     },
+  //   ],
+  // };
+
+  // const options = {
+  //   // responsive: true,
+  //   maintainAspectRatio: false,
+  //   plugins: {
+  //     legend: { position: "top" },
+  //     title: { display: true, text: `${company} STATISTICS` },
+  //   },
+  // };
+
   return (
     <>
-      <PageHeader title="Dashboard" />
-      <ol className="breadcrumb float-xl-right">
-        <li className="breadcrumb-item">
-          <a href="javascript:;">Home</a>
-        </li>
-        <li className="breadcrumb-item">
-          <a href="javascript:;">Library</a>
-        </li>
-        <li className="breadcrumb-item active">
-          <a href="javascript:;">Data</a>
-        </li>
-      </ol>
-
-      <h1 className="page-header">
-        Dashboard <small>header small text goes here...</small>
-      </h1>
-
       <div className="row">
-        {/* begin col-3 */}
+        {/* Job Postings */}
         <div className="col-xl-3 col-md-6">
-          <div className="widget widget-stats bg-white text-inverse">
-            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
-              <i className="ion-ios-person-add"></i>
-            </div>
-            <div className="stats-content">
-              <div className="text-inverse font-weight-bold">Users</div>
-              <div className="stats-number">7,842,900</div>
-              <div className="stats-progress progress">
-                <div className="progress-bar" style={{ width: "70.1%" }}></div>
-              </div>
-              <div className="stats-desc text-inverse-lighter">
-                Better than last week (70.1%)
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* end col-3 */}
-
-        {/* begin col-3 */}
-        <div className="col-xl-3 col-md-6">
-          <div className="widget widget-stats bg-white text-inverse">
-            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
-              <i className="ion-ios-business"></i>
-            </div>
-            <div className="stats-content">
-              <div className="text-inverse font-weight-bold">Companies</div>
-              <div className="stats-number">180,200</div>
-              <div className="stats-progress progress">
-                <div className="progress-bar" style={{ width: "40.5%" }}></div>
-              </div>
-              <div className="stats-desc text-inverse-lighter">
-                Better than last week (40.5%)
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* end col-3 */}
-
-        {/* begin col-3 */}
-        <div className="col-xl-3 col-md-6">
-          <div className="widget widget-stats bg-white text-inverse">
-            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+          <div className="widget widget-stats bg-warning">
+            <div className="stats-icon stats-icon-square bg-custom text-white">
               <i className="ion-ios-create"></i>
             </div>
-            <div className="stats-content">
-              <div className="text-inverse font-weight-bold">Job Postings</div>
-              <div className="stats-number">38,900</div>
-              <div className="stats-progress progress">
-                <div className="progress-bar" style={{ width: "76.3%" }}></div>
-              </div>
-              <div className="stats-desc text-inverse-lighter">
-                Better than last week (76.3%)
-              </div>
+            <div className="stats-info">
+              <h4 className="text-white">TOTAL USERS</h4>
+              <p>{formatCount(dashboardData?.data?.total_users)}</p>
+            </div>
+            <div className="stats-link">
+              <Link to="/home/users">
+                View Detail <i className="fa fa-arrow-alt-circle-right"></i>
+              </Link>
             </div>
           </div>
         </div>
-        {/* end col-3 */}
 
-        {/* begin col-3 */}
+        {/* Applicants */}
         <div className="col-xl-3 col-md-6">
-          <div className="widget widget-stats bg-white text-inverse">
-            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+          <div className="widget widget-stats bg-info">
+            <div className="stats-icon stats-icon-square bg-custom text-white">
               <i className="ion-ios-walk"></i>
             </div>
-            <div className="stats-content">
-              <div className="text-inverse font-weight-bold">Applicants</div>
-              <div className="stats-number">3,988</div>
-              <div className="stats-progress progress">
-                <div className="progress-bar" style={{ width: "54.9%" }}></div>
-              </div>
-              <div className="stats-desc text-inverse-lighter">
-                Better than last week (54.9%)
-              </div>
+            <div className="stats-info">
+              <h4 className="text-white">TOTAL COMPANIES</h4>
+              <p>{formatCount(dashboardData?.data?.total_companies)}</p>
+            </div>
+            <div className="stats-link">
+              <Link to="/home/companies">
+                View Detail <i className="fa fa-arrow-alt-circle-right"></i>
+              </Link>
             </div>
           </div>
         </div>
-        {/* end col-3 */}
-      </div>
 
-      <div className="panel panel-inverse">
-        <div className="panel-heading">
-          <h4 className="panel-title">Panel Title here</h4>
-          <div className="panel-heading-btn">
-            <a
-              href="javascript:;"
-              className="btn btn-xs btn-icon btn-circle btn-default"
-              data-click="panel-expand"
-            >
-              <i className="fa fa-expand"></i>
-            </a>
-            <a
-              href="javascript:;"
-              className="btn btn-xs btn-icon btn-circle btn-success"
-              data-click="panel-reload"
-            >
-              <i className="fa fa-redo"></i>
-            </a>
-            <a
-              href="javascript:;"
-              className="btn btn-xs btn-icon btn-circle btn-warning"
-              data-click="panel-collapse"
-            >
-              <i className="fa fa-minus"></i>
-            </a>
-            <a
-              href="javascript:;"
-              className="btn btn-xs btn-icon btn-circle btn-danger"
-              data-click="panel-remove"
-            >
-              <i className="fa fa-times"></i>
-            </a>
+        {/* Hired Applicants */}
+        <div className="col-xl-3 col-md-6">
+          <div className="widget widget-stats bg-danger">
+            <div className="stats-icon stats-icon-square bg-custom text-white">
+              <i className="ion-ios-checkmark-circle"></i>
+            </div>
+            <div className="stats-info">
+              <h4 className="text-white">TOTAL JOB POSTINGS</h4>
+              <p>{formatCount(dashboardData?.data?.total_job_postings)}</p>
+            </div>
+            <div className="stats-link">
+              <Link to="/home/job-postings">
+                View Detail <i className="fa fa-arrow-alt-circle-right"></i>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="panel-body">Panel Content Here</div>
+
+        {/* Rejected Applicants */}
+        <div className="col-xl-3 col-md-6">
+          <div className="widget widget-stats bg-purple">
+            <div className="stats-icon stats-icon-square bg-custom text-white">
+              <i className="ion-ios-close-circle"></i>
+            </div>
+            <div className="stats-info">
+              <h4 className="text-white">TOTAL APPLICANTS</h4>
+              <p>{formatCount(dashboardData?.data?.total_applicants)}</p>
+            </div>
+            <div className="stats-link pe-none">
+              <Link className="opacity-0" to="/home/applicants">
+                View Detail <i className="fa fa-arrow-alt-circle-right"></i>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Panel with Chart */}
+      <div className="row">
+        <div className="col-sm-12 col-xl-6 col-md-6">
+          {/* <Panel title="Bar Chart Analytics">
+            <div className="d-flex align-items-center justify-content-center">
+              <div
+                style={{ width: "400px", height: "400px", margin: "0 auto" }}
+              >
+                <Bar
+                  options={options}
+                  data={barChartData}
+                  className="pie-chart"
+                />
+              </div>
+            </div>
+          </Panel> */}
+        </div>
+        <div className="col-sm-12 col-xl-6 col-md-6">
+          {/* <Panel title="Pie Chart Analytics">
+            <div className="d-flex align-items-center justify-content-center">
+              <div
+                style={{ width: "400px", height: "400px", margin: "0 auto" }}
+              >
+                <Pie data={pieChartData} />
+              </div>
+            </div>
+          </Panel> */}
+        </div>
       </div>
     </>
   );
