1c950adfcdd800e25726fa506b21963636c602f5
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-05-13 16:20:19 +0800

    feat: display recent_applicants,users,companies and job postings.

--- src/pages/Dashboard/components/MainDashboard.jsx
+++ src/pages/Dashboard/components/MainDashboard.jsx
@@ -3,8 +3,9 @@ import Panel from "@/components/UI/Panel";
 import AuthContext from "@/store/auth/auth-context";
 import { Link } from "react-router-dom";
 import useFetch from "@/hooks/useFetch";
-import { formatCount } from "@/libs/utils";
+import { formatCount, formatDateTime } from "@/libs/utils";
 import { Bar, Pie } from "react-chartjs-2";
+import { formatDate } from "@/libs/utils";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -90,17 +91,19 @@ const MainDashboard = () => {
       <div className="row">
         {/* Job Postings */}
         <div className="col-xl-3 col-md-6">
-          <div class="widget widget-stats bg-white text-inverse shadow-lg">
-            <div class="stats-icon stats-icon-square bg-gradient-blue text-white">
-              <i class="ion-ios-person"></i>
+          <div className="widget widget-stats bg-white text-inverse shadow-lg">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-person"></i>
             </div>
-            <div class="stats-content">
-              <div class="stats-title text-inverse-lighter">Total Users</div>
-              <div class="stats-number">
+            <div className="stats-content">
+              <div className="stats-title text-inverse-lighter">
+                Total Users
+              </div>
+              <div className="stats-number">
                 {formatCount(dashboardData?.data?.total_users)}
               </div>
-              <div class="stats-progress progress">
-                <div class="progress-bar" style={{ width: "100%" }}></div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "100%" }}></div>
               </div>
               <div className="stats-link">
                 <Link to="/home/users">
@@ -112,19 +115,19 @@ const MainDashboard = () => {
         </div>
 
         <div className="col-xl-3 col-md-6">
-          <div class="widget widget-stats bg-white text-inverse shadow-lg">
-            <div class="stats-icon stats-icon-square bg-gradient-blue text-white">
-              <i class="ion-ios-business"></i>
+          <div className="widget widget-stats bg-white text-inverse shadow-lg">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-business"></i>
             </div>
-            <div class="stats-content">
-              <div class="stats-title text-inverse-lighter">
+            <div className="stats-content">
+              <div className="stats-title text-inverse-lighter">
                 Total Companies
               </div>
-              <div class="stats-number">
+              <div className="stats-number">
                 {formatCount(dashboardData?.data?.total_companies)}
               </div>
-              <div class="stats-progress progress">
-                <div class="progress-bar" style={{ width: "100%" }}></div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "100%" }}></div>
               </div>
               <div className="stats-link">
                 <Link to="/home/companies">
@@ -136,19 +139,19 @@ const MainDashboard = () => {
         </div>
 
         <div className="col-xl-3 col-md-6">
-          <div class="widget widget-stats bg-white text-inverse shadow-lg">
-            <div class="stats-icon stats-icon-square bg-gradient-blue text-white">
-              <i class="ion-ios-create"></i>
+          <div className="widget widget-stats bg-white text-inverse shadow-lg">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-create"></i>
             </div>
-            <div class="stats-content">
-              <div class="stats-title text-inverse-lighter">
+            <div className="stats-content">
+              <div className="stats-title text-inverse-lighter">
                 Total Job Postings
               </div>
-              <div class="stats-number">
+              <div className="stats-number">
                 {formatCount(dashboardData?.data?.total_job_postings)}
               </div>
-              <div class="stats-progress progress">
-                <div class="progress-bar" style={{ width: "100%" }}></div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "100%" }}></div>
               </div>
               <div className="stats-link">
                 <Link to="/home/job-postings">
@@ -160,19 +163,19 @@ const MainDashboard = () => {
         </div>
 
         <div className="col-xl-3 col-md-6">
-          <div class="widget widget-stats bg-white text-inverse shadow-lg">
-            <div class="stats-icon stats-icon-square bg-gradient-blue text-white">
-              <i class="ion-ios-walk"></i>
+          <div className="widget widget-stats bg-white text-inverse shadow-lg">
+            <div className="stats-icon stats-icon-square bg-gradient-blue text-white">
+              <i className="ion-ios-walk"></i>
             </div>
-            <div class="stats-content">
-              <div class="stats-title text-inverse-lighter">
+            <div className="stats-content">
+              <div className="stats-title text-inverse-lighter">
                 Total Applicants
               </div>
-              <div class="stats-number">
+              <div className="stats-number">
                 {formatCount(dashboardData?.data?.total_applicants)}
               </div>
-              <div class="stats-progress progress">
-                <div class="progress-bar" style={{ width: "100%" }}></div>
+              <div className="stats-progress progress">
+                <div className="progress-bar" style={{ width: "100%" }}></div>
               </div>
               <div className="stats-link pe-none opacity-0">
                 <Link to="/home/users">
@@ -186,7 +189,7 @@ const MainDashboard = () => {
 
       {/* Panel with Chart */}
       <div className="row">
-        <div className="col-sm-12 col-xl-6 col-md-6">
+        <div className="col-sm-12 col-xl-6 col-md-6 mb-4">
           <Panel title="Bar Chart Analytics">
             <div className="d-flex align-items-center justify-content-center">
               <div
@@ -201,13 +204,249 @@ const MainDashboard = () => {
             </div>
           </Panel>
         </div>
-        <div className="col-sm-12 col-xl-6 col-md-6">
+        <div className="col-sm-12 col-xl-6 col-md-6 mb-4">
           <Panel title="Pie Chart Analytics">
             <div className="d-flex align-items-center justify-content-center">
               <div
                 style={{ width: "400px", height: "400px", margin: "0 auto" }}
               >
                 <Pie data={pieChartData} />
+              </div>
+            </div>
+          </Panel>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-12 col-xl-6 col-md-6 mb-4">
+          <Panel title="Recently Registered Companies">
+            <div className="row">
+              <div className="col-12">
+                <div className="table-responsive">
+                  <table className="table table-striped table-bordered table-td-valign-middle dataTable no-footer dtr-inline collapsed">
+                    <thead>
+                      <tr>
+                        <th className="text-center font-weight-bold fs-7">
+                          No.
+                        </th>
+                        <th className="text-center font-weight-bold fs-7">
+                          Full Name
+                        </th>
+                        {/* <th className="text-center font-weight-bold fs-7">
+                          Email
+                        </th> */}
+                        <th className="text-center font-weight-bold fs-7">
+                          Date Registered
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dashboardData?.data?.recent_users?.length > 0 ? (
+                        dashboardData.data.recent_users.map((d, index) => (
+                          <tr key={d.id}>
+                            <td className="text-center font-weight-bold fs-7">
+                              {index + 1}
+                            </td>
+                            <td className="text-center font-weight-bold fs-7">
+                              {`${d.lastname}, ${d.firstname} ${d.middlename}`}
+                            </td>
+                            {/* <td className="text-center font-weight-bold fs-7">
+                              {d.email}
+                            </td> */}
+                            <td className="text-center font-weight-bold fs-7">
+                              {formatDateTime(d.created_at)}
+                            </td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td
+                            colSpan="4"
+                            className="text-center font-weight-bold fs-7"
+                          >
+                            No data found
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </Panel>
+        </div>
+
+        <div className="col-sm-12 col-xl-6 col-md-6 mb-4">
+          <Panel title="Recently Registered Companies">
+            <div className="row">
+              <div className="col-12">
+                <div className="table-responsive">
+                  <table className="table table-striped table-bordered table-td-valign-middle dataTable no-footer dtr-inline collapsed">
+                    <thead>
+                      <tr>
+                        <th className="text-center font-weight-bold fs-7">
+                          No.
+                        </th>
+                        <th className="text-center font-weight-bold fs-7">
+                          Name
+                        </th>
+                        {/* <th className="text-center font-weight-bold fs-7">
+                          Email
+                        </th> */}
+                        <th className="text-center font-weight-bold fs-7">
+                          Date Registered
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dashboardData?.data?.recent_companies?.length > 0 ? (
+                        dashboardData.data.recent_companies.map((d, index) => (
+                          <tr key={d.id}>
+                            <td className="text-center font-weight-bold fs-7">
+                              {index + 1}
+                            </td>
+                            <td className="text-center font-weight-bold fs-7">
+                              {d.name}
+                            </td>
+                            {/* <td className="text-center font-weight-bold fs-7">
+                              {d.email}
+                            </td> */}
+                            <td className="text-center font-weight-bold fs-7">
+                              {formatDateTime(d.created_at)}
+                            </td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td
+                            colSpan="4"
+                            className="text-center font-weight-bold fs-7"
+                          >
+                            No data found
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </Panel>
+        </div>
+
+        <div className="col-sm-12 col-xl-6 col-md-6 mb-4">
+          <Panel title="Recently Job Postings">
+            <div className="row">
+              <div className="col-12">
+                <div className="table-responsive">
+                  <table className="table table-striped table-bordered table-td-valign-middle dataTable no-footer dtr-inline collapsed">
+                    <thead>
+                      <tr>
+                        <th className="text-center font-weight-bold fs-7">
+                          No.
+                        </th>
+                        <th className="text-center font-weight-bold fs-7">
+                          Name
+                        </th>
+                        {/* <th className="text-center font-weight-bold fs-7">
+                          Email
+                        </th> */}
+                        <th className="text-center font-weight-bold fs-7">
+                          Date Registered
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dashboardData?.data?.recent_job_postings?.length > 0 ? (
+                        dashboardData.data.recent_job_postings.map(
+                          (d, index) => (
+                            <tr key={d.id}>
+                              <td className="text-center font-weight-bold fs-7">
+                                {index + 1}
+                              </td>
+                              <td className="text-center font-weight-bold fs-7">
+                                {d.title}
+                              </td>
+                              {/* <td className="text-center font-weight-bold fs-7">
+                              {d.email}
+                            </td> */}
+                              <td className="text-center font-weight-bold fs-7">
+                                {formatDateTime(d.created_at)}
+                              </td>
+                            </tr>
+                          )
+                        )
+                      ) : (
+                        <tr>
+                          <td
+                            colSpan="4"
+                            className="text-center font-weight-bold fs-7"
+                          >
+                            No data found
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </Panel>
+        </div>
+
+        <div className="col-sm-12 col-xl-6 col-md-6 mb-4">
+          <Panel title="Recently Applicants">
+            <div className="row">
+              <div className="col-12">
+                <div className="table-responsive">
+                  <table className="table table-striped table-bordered table-td-valign-middle dataTable no-footer dtr-inline collapsed">
+                    <thead>
+                      <tr>
+                        <th className="text-center font-weight-bold fs-7">
+                          No.
+                        </th>
+                        <th className="text-center font-weight-bold fs-7">
+                          Name
+                        </th>
+                        <th className="text-center font-weight-bold fs-7">
+                          Job Posting Applied
+                        </th>
+                        <th className="text-center font-weight-bold fs-7">
+                          Date Applied
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dashboardData?.data?.recent_applicants?.length > 0 ? (
+                        dashboardData.data.recent_applicants.map((d, index) => (
+                          <tr key={d.id}>
+                            <td className="text-center font-weight-bold fs-7">
+                              {index + 1}
+                            </td>
+                            <td className="text-center font-weight-bold fs-7">
+                              {`${d?.user?.lastname}, ${d?.user?.firstname} ${d?.user?.middlename}`}
+                            </td>
+                            <td className="text-center font-weight-bold fs-7">
+                              {d?.job_posting?.title}
+                            </td>
+                            <td className="text-center font-weight-bold fs-7">
+                              {formatDateTime(d.created_at)}
+                            </td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td
+                            colSpan="4"
+                            className="text-center font-weight-bold fs-7"
+                          >
+                            No data found
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </Panel>
