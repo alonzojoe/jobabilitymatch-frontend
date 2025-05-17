f35199689d10b05f0a17fe7bbfb3fd65ad6760d6
alonzojoe <alonzojoenell2k15@gmail.com>
2025-05-17 20:55:14 +0800

    style: create ui ux for Company Details

--- src/pages/Feed/CompanyDetails.jsx
+++ src/pages/Feed/CompanyDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import useFetch from "@/hooks/useFetch";
 import { capitalized } from "@/libs/utils";
+import { FaUserTie } from "react-icons/fa";
+import { MdContactPhone } from "react-icons/md";
 const CompanyDetails = () => {
   const { id: company_id } = useParams();
 
@@ -12,16 +14,60 @@ const CompanyDetails = () => {
 
   const company = details?.data;
 
+  const employer = `${company?.user?.lastname}, ${company?.user.firstname} ${company?.user.middlename}`;
+
   return (
     <>
-      <div className="mt-3  ">
-        <h2 className="d-flex gap-2">
-          <i className="ti ti-buildings"></i>
-          {capitalized(company?.name)}
+      <div className="cmp-container">
+        <h2 className="d-flex align-items-center">
+          <i className="ti ti-buildings" style={{ fontSize: "5rem" }}></i>
+          <div>
+            <span className="d-block mb-0">{capitalized(company?.name)}</span>
+            <span className="fs-5 fw-500 text-muted color-custom">
+              {capitalized(company?.address)}
+            </span>
+          </div>
         </h2>
-      </div>
-      <div className="rec-container">
-        <h3 className="fw-bold mb-0 pb-2 text-center rec-text cursor-pointer"></h3>
+
+        <div className="d-flex gap-3 mt-3">
+          <h4 className="fw-bold mb-0 pb-2 text-center rec-text cursor-pointer active">
+            Job Postings ({company?.job_postings_count})
+          </h4>
+        </div>
+        <div className="mt-4">
+          <div className="cmp-details-container mt-5">
+            <div>
+              <h3 className="text-dark">About the company:</h3>
+
+              <span className="d-block d-flex align-items-center gap-1 fs-6 ml-3">
+                <i className="ti ti-buildings"></i>
+                {capitalized(company?.name)}
+              </span>
+              <span className="d-block d-flex align-items-center gap-1 fs-6 text-capitalize ml-3">
+                <i className="ti ti-map-pin"></i>
+                {capitalized(company?.address)}
+              </span>
+            </div>
+
+            <div>
+              <h3 className="text-dark">Employer details:</h3>
+
+              <span className="d-block d-flex align-items-center gap-1 fs-6 ml-3">
+                <FaUserTie />
+                {capitalized(employer)}
+              </span>
+              <span className="d-block d-flex align-items-center gap-1 fs-6 text-capitalize ml-3">
+                <MdContactPhone />
+                {`${company?.user?.phone} / ${company?.user.email}`}
+              </span>
+            </div>
+          </div>
+
+          <h3 className="text-dark mt-5">Jobs</h3>
+          <div className="row">
+            <div className="col-md-12"></div>
+          </div>
+        </div>
       </div>
     </>
   );
