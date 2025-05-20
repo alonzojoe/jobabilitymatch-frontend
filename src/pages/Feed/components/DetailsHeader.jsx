1acb78114c3641b67b9c47b69716308e7ed7a28a
alonzojoe <alonzojoenell2k15@gmail.com>
2025-05-21 00:12:18 +0800

    feat: add back button in CompanyDetails using navigate(-1)

--- src/pages/Feed/components/DetailsHeader.jsx
+++ src/pages/Feed/components/DetailsHeader.jsx
@@ -1,16 +1,29 @@
 import { capitalized } from "@/libs/utils";
+import { ImArrowLeft } from "react-icons/im";
+import { useNavigate } from "react-router-dom";
 
 const DetailsHeader = ({ company }) => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <h2 className="d-flex align-items-center gap-2">
-        <i className="ti ti-buildings" style={{ fontSize: "5rem" }}></i>
-        <div>
-          <span className="d-block mb-0">{capitalized(company?.name)}</span>
-          <span className="fs-5 fw-500 text-muted color-custom">
-            {capitalized(company?.address)}
-          </span>
+      <h2 className="d-flex justify-content-between align-items-center gap-2">
+        <div className="d-flex align-items-center gap-2">
+          <i className="ti ti-buildings" style={{ fontSize: "5rem" }}></i>
+          <div>
+            <span className="d-block mb-0">{capitalized(company?.name)}</span>
+            <span className="fs-5 fw-500 text-muted color-custom">
+              {capitalized(company?.address)}
+            </span>
+          </div>
         </div>
+        <button
+          className="btn btn-custom btn-lg d-flex align-items-center gap-2"
+          onClick={() => navigate(-1)}
+        >
+          <ImArrowLeft />
+          Back to Job Postings
+        </button>
       </h2>
 
       <div className="d-flex gap-3 mt-3">
